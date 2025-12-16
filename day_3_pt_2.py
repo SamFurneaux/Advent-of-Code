0a8216757edcc05c9db1f1772c7f5b400bc97e77
Sam <samuel.furneaux@gmail.com>
2025-12-16 15:28:26 +0000

    Add files via upload

--- day_3_pt_2.py
+++ day_3_pt_2.py
@@ -4,12 +4,28 @@ with open("day_3_input.txt", "r", encoding="utf-8") as f:
     batteries = [line.strip() for line in f]
 
 
-test = ['811111111111119']#'987654321111111', '811111111111119' ,'234234234234278', '818181911112111']
+#test = ['987654321111111', '811111111111119', '234234234234278', '818181911112111']
 
+num_list = []
+for battery in batteries:
+    
+    ##bat = [int(num) for num in battery]
+    bat = np.array(list(map(int, battery)))
+    idxs = np.argsort(-bat, kind="stable")
 
-for battery in test:
-    bat = [int(num) for num in battery]
-    print(np.argsort(bat))
-    remainder = 100 
-    #while remainder < 12:
+    ##idxs = np.argsort(bat)[::-1]
+    num = []
+    remainder = 12
+    while remainder >0:
+        for i in idxs:
+            if (100-i) >= remainder:
+                num.append(bat[i])
+                #idxs = idxs[idxs != i]
+                idxs = idxs[idxs >i]
+                remainder -=1
+                break
+            else:
+                continue
+    num_list.append(int(''.join(str(x) for x in num)))
 
+print(sum(num_list))
